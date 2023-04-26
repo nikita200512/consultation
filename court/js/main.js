8a4dcf22bf1c0472b5194565d3c8bdbbf1a9ea40
nikita200512 <93154095+nikita200512@users.noreply.github.com>
2023-04-26 20:38:47 +0300

    Add files via upload

---
+++ court/js/main.js
@@ -0,0 +1,7 @@
+$('.reviews__items').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button><img src="images/arrows/prev-arrow.svg" alt=""></button>',
+  nextArrow: '<button><img src="images/arrows/next-arrow.svg" alt=""></button>'
+});
